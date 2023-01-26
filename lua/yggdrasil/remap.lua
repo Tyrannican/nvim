e4b7eca6c0e862917632a9de41a77c290e60ed02
Graham Keenan <graham.keenan@outlook.com>
2023-01-26 20:07:15 +0000

    update mappings

--- lua/yggdrasil/remap.lua
+++ lua/yggdrasil/remap.lua
@@ -30,3 +30,19 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-N>")
+
+-- Use the home row, bro
+vim.keymap.set("n", "<Left>", "<nop>")
+vim.keymap.set("n", "<Right>", "<nop>")
+vim.keymap.set("n", "<Up>", "<nop>")
+vim.keymap.set("n", "<Down>", "<nop>")
+
+vim.keymap.set("i", "<Left>", "<nop>")
+vim.keymap.set("i", "<Right>", "<nop>")
+vim.keymap.set("i", "<Up>", "<nop>")
+vim.keymap.set("i", "<Down>", "<nop>")
+
+vim.keymap.set("v", "<Left>", "<nop>")
+vim.keymap.set("v", "<Right>", "<nop>")
+vim.keymap.set("v", "<Up>", "<nop>")
+vim.keymap.set("v", "<Down>", "<nop>")
