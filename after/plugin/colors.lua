893d1fbf937e3c0a8c8a989b6c4b9ecb8c3c044e
Graham Keenan <graham.keenan@outlook.com>
2023-05-29 12:57:42 +0100

    transparent background yo

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -16,7 +16,8 @@ function Colors(color)
 	color = color or "kyotonight"
     KyotoNight()
 	vim.cmd.colorscheme(color)
-
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 Colors()
